41439709df3e77dbd3267d18b1a138f33334a47b
Code-Warlock <70621824+Code-Warlock@users.noreply.github.com>
2023-06-07 19:50:18 +0000

    fixed static

--- now_on/settings.py
+++ now_on/settings.py
@@ -114,14 +114,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'main_app/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILESDIRS = [
-     BASE_DIR / 'static'
+STATICFILES_DIRS = [
+     BASE_DIR / 'main_app/static'
 ]
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'main_app/static')

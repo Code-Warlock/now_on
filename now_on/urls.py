949d5df0872a6e3f25893d151885719c5571054c
Code-Warlock <70621824+Code-Warlock@users.noreply.github.com>
2023-06-07 19:42:54 +0000

    vercel completion

--- now_on/urls.py
+++ now_on/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from django.config.urls.static import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
